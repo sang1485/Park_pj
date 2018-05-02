df675f0a35510f0d2941f690edefaa56ae0926c2
Park <sang14855@gmail.com>
2018-05-02 11:22:33 +0900

    LED 등록

---
+++ test_0502/test_0502/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * test_0502.cpp
+ *
+ * Created: 2018-05-02 오전 10:52:05
+ * Author : stc_161
+ */ 
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+    PORTD = 0xff;
+	DDRD = 0xff;
+	/* Replace with your application code */
+    while (1) 
+    {
+		PORTD = 0x00;
+		_delay_ms(500);
+		PORTD = 0xff;
+		_delay_ms(500);
+    }
+}
+
